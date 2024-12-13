618accea87b73af3d188406f924a0957a6acd2e1
Andrey Luiz <andrey@timeleap.swiss>
2024-12-13 15:50:36 +0100

    TLS-22 Add serializar functions to Python library

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="timeleap-sia",
-    version="1.0.1",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[],
     author="Pouya Eghbali",
--- sia/__init__.py
+++ sia/__init__.py
@@ -1,3 +1,4 @@
 from .sia import Sia
+from .serializers import *
 
-__all__ = ["Sia"]
+__all__ = ["Sia", "serialize_int8_array_item", "serialize_int16_array_item", "serialize_int32_array_item", "serialize_int64_array_item", "serialize_uint8_array_item", "serialize_uint16_array_item", "serialize_uint32_array_item", "serialize_uint64_array_item", "serialize_string8_array_item", "serialize_string16_array_item", "serialize_string32_array_item", "serialize_string64_array_item", "serialize_byte_array8_array_item", "serialize_byte_array16_array_item", "serialize_byte_array32_array_item", "serialize_byte_array64_array_item", "serialize_bool_array_item"]
